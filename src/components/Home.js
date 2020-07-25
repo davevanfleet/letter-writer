d535ac11178f20c4fc35e8da1b5153770c0d2532
dvfleet413 <dvfleet413@gmail.com>
2020-07-25 13:23:12 -0400

    updated Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Home = (props) => {
 
     return(
         <>
-            <h1>Yooooo!</h1>
+            <h1>Letter Writer</h1>
+            <div className="home">
+                <p>Welcome to Letter Writer, used to maintain letter writing territories and our do-not-call-list.</p>
+                <ul>
+                    <li><LinkContainer to="/territories"><Nav.Link>View Letter Writing Territories</Nav.Link></LinkContainer></li>
+                    <li><LinkContainer to="/DNCs"><Nav.Link>View/Maintain DNC List</Nav.Link></LinkContainer></li>
+                </ul>
+            </div>
         </>
     )
 }
