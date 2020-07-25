f76616de1539e55cacdd95a4c41d5bd841073974
dvfleet413 <dvfleet413@gmail.com>
2020-07-25 19:00:39 -0400

    refactored Territory into function component

--- src/components/Territory.js
+++ src/components/Territory.js
@@ -1,59 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import uuid from 'uuid';
 import { CSVLink } from "react-csv";
 import CheckBox from './CheckBox';
+import { config } from '../constants';
 
-class Territory extends Component {
+const Territory = (props) => {
+    const [dncs, setDncs] = useState()
+    useEffect(() => {
+        fetch(`${config.url.API_URL}/territories/${props.territoryId}/dncs`)
+            .then(r => r.json())
+            .then(d => setDncs(d))
+    }, [props.territoryId])
 
-    render(){
-        const headers = [
-            { label: "Name", key: "name" },
-            { label: "Address", key: "address" },
-            { label: "Phone", key: "phone" },
-            { label: "Check When Finished", key: "blank"}
-          ]
-        
-        const contacts = this.props.contacts.map(contact => <tr key={uuid()}><td>{contact.name}</td><td>{contact.address}</td><td>{contact.phone}</td><CheckBox id={contact.id} /></tr>)
-        
-        return(
-            <div>
-                <p>{this.props.contacts.length} Contacts Loaded</p>
-                <CSVLink data={this.props.contacts} 
-                         headers={headers}
-                         filename={`${this.props.name}.csv`}
-                         className="btn btn-primary">
-                             Download Territory
-                </CSVLink>
-                <h2>Contacts</h2>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Name:</th>
-                            <th>Address:</th>
-                            <th>Phone:</th>
-                            <th>Check When Done:</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { contacts }
-                    </tbody>
-                </Table>
-                <h2>DNCs</h2>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Address:</th>
-                            <th>Date:</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        
-                    </tbody>
-                </Table>
-            </div>
-        )
-    }
+    const headers = [
+        { label: "Name", key: "name" },
+        { label: "Address", key: "address" },
+        { label: "Phone", key: "phone" },
+        { label: "Check When Finished", key: "blank"}
+      ]
+    
+    const contacts = props.contacts.map(contact => <tr key={uuid()}><td>{contact.name}</td><td>{contact.address}</td><td>{contact.phone}</td><CheckBox id={contact.id} /></tr>)
+    
+    return(
+        <div>
+            <p>{props.contacts.length} Contacts Loaded</p>
+            <CSVLink data={props.contacts} 
+                     headers={headers}
+                     filename={`${props.name}.csv`}
+                     className="btn btn-primary">
+                         Download Territory
+            </CSVLink>
+            <h2>Contacts</h2>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Name:</th>
+                        <th>Address:</th>
+                        <th>Phone:</th>
+                        <th>Check When Done:</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { contacts }
+                </tbody>
+            </Table>
+            <h2>DNCs</h2>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Address:</th>
+                        <th>Date:</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </Table>
+        </div>
+    )
 }
 
 export default Territory
