6bec1d8c59b09d3096518f9dd84a4023b94e0a38
davevanfleet <vanfleet@arsome.com>
2021-03-21 19:22:55 -0400

    refactored Territories component into function

--- src/containers/Territories.js
+++ src/containers/Territories.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import {Map, Polygon, GoogleApiWrapper} from 'google-maps-react';
@@ -6,31 +6,36 @@ import { config } from '../constants';
 import uuid from 'uuid';
 import Territory from '../components/Territory';
 
-class Territories extends Component {
-    state = {
-        territoryId: 0,
-        territoryName: '',
-        territories: [],
-        path: [],
-        contacts: [],
-        contactsLoaded: false,
-    }
+const Territories = (props) => {
+    // state = {
+    //     territoryId: 0,
+    //     territoryName: '',
+    //     territories: [],
+    //     path: [],
+    //     contacts: [],
+    //     contactsLoaded: false,
+    // }
 
-    componentDidMount(){
+    const [ territoryId, setTerritoryId ] = useState(0)
+    const [ territoryName, setTerritoryName ] = useState('')
+    const [ territories, setTerritories ] = useState([])
+    const [ path, setPath ] = useState([])
+    const [ contacts, setContacts ] = useState([])
+    const [ contactsLoaded, setContactsLoaded ] = useState(false)
+
+    useEffect(() => {
         fetch(`${config.url.API_URL}/territories`)
             .then(r => r.json())
             .then(data => {
-                this.setState({
-                    territories: data
-                })
+                setTerritories(data)
             })
-    }
+    }, [])
 
-    handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault()
     }
 
-    getCenter = (points) => {
+    const getCenter = (points) => {
         const lngCenter = points.reduce((sum, point) => {
             return sum + point.lng
         }, 0) / points.length
@@ -42,80 +47,72 @@ class Territories extends Component {
         return {lng: lngCenter, lat: latCenter}
     }
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value,
-            contactsLoaded: false
-        })
+    const handleChange = (e) => {
+        setTerritoryId(e.target.value)
+        setContactsLoaded(false)
 
         const filterContacts = (polygon) => {
             fetch(`${config.url.API_URL}/contacts`)
                 .then(r => r.json())
                 .then(d => {
                     const filteredList = d.filter(contact => {
-                        const coords = new this.props.google.maps.LatLng({lat: contact.lat, lng: contact.lng})
-                        return this.props.google.maps.geometry.poly.containsLocation(coords, polygon)
+                        const coords = new props.google.maps.LatLng({lat: contact.lat, lng: contact.lng})
+                        return props.google.maps.geometry.poly.containsLocation(coords, polygon)
                     })
-                    this.setState({
-                        contacts: filteredList,
-                        contactsLoaded: true
-                    })
+                    setContacts(filteredList)
+                    setContactsLoaded(true)
                 })
         }
 
         fetch(`${config.url.API_URL}/territories/${e.target.value}`)
                 .then(r => r.json())
                 .then(d => {
-                    const territory = new this.props.google.maps.Polygon({paths: d.points})
-                    this.setState({
-                        territory: territory,
-                        territoryName: d.name
-                    })
+                    const territory = new props.google.maps.Polygon({paths: d.points})
+                    // setTerritory(territory)
+                    setTerritoryName(d.name)
                     filterContacts(territory)
-                    this.setState({
-                        path: d.points
-                    })
+                    setPath(d.points)
                 })
     }
-
-    render(){
-        const territories = this.state.territories.sort((a, b) => {return (a.name < b.name ? -1 : 1)}).map(t => <option key={uuid()} value={t.id}>{t.name}</option>)
-        return(
-            <div>
-                <h3>Select a Territory</h3>
-                <select name="territoryId" onChange={this.handleChange} value={this.state.territoryId}>
-                    <option key={uuid()} value="0">Select a Territory</option>
-                    {territories}
-                </select>
-                {this.state.contactsLoaded ? 
-                                            <>
-                                                <Map google={this.props.google}
-                                                     style={{width: '100%', height: "400px", position: 'relative'}}
-                                                     className={'map'}
-                                                     initialCenter={this.getCenter(this.state.path)}
-                                                     zoom={14}>
-                                                    <Polygon paths={this.state.path}
-                                                             strokeColor="#0000FF"
-                                                             strokeOpacity={0.8}
-                                                             strokeWeight={2}
-                                                             fillColor="#0000FF"
-                                                             fillOpacity={0.35}/>
-                                                </Map>
-                                                <Territory contacts={this.state.contacts}
-                                                           name={this.state.territoryName}
-                                                           territoryId={this.state.territoryId} />
-                                            </>
-                                            : 
-                                            (this.state.territoryId > 0 ? 
-                                                <>
-                                                    <br /><br />
-                                                    <FontAwesomeIcon icon={faSpinner} size="6x" spin />
-                                                </> 
-                                                :
-                                                null)}
-            </div>
-        )
-    }
+        
+    const territoryOptions = territories.sort((a, b) => {return (a.name < b.name ? -1 : 1)}).map(t => <option key={uuid()} value={t.id}>{t.name}</option>)
+        
+    return (
+        <div>
+            <h3>Select a Territory</h3>
+            <select name="territoryId" onChange={handleChange} value={territoryId}>
+                <option key={uuid()} value="0">Select a Territory</option>
+                {territoryOptions}
+            </select>
+            {contactsLoaded ? 
+            <>
+                <Map google={props.google}
+                     style={{width: '100%', height: "400px", position: 'relative'}}
+                     className={'map'}
+                     initialCenter={getCenter(path)}
+                     zoom={14}>
+                    <Polygon paths={path}
+                             strokeColor="#0000FF"
+                             strokeOpacity={0.8}
+                             strokeWeight={2}
+                             fillColor="#0000FF"
+                             fillOpacity={0.35}
+                             editable/>
+                </Map>
+                <Territory contacts={contacts}
+                           name={territoryName}
+                           territoryId={territoryId} />
+            </>
+            : 
+            (territoryId > 0 ? 
+                <>
+                    <br /><br />
+                    <FontAwesomeIcon icon={faSpinner} size="6x" spin />
+                </> 
+                :
+                null)}
+        </div>
+    )
 }
 
 export default GoogleApiWrapper({
