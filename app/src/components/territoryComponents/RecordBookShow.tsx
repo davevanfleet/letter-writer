40dd2a2913cc8eba58de75d4ae403fbcbce59cf9
davevanfleet <vanfleet@arsome.com>
2021-10-28 23:25:05 -0400

    switched pdf to download instead of viewer

--- app/src/components/territoryComponents/RecordBookShow.tsx
+++ app/src/components/territoryComponents/RecordBookShow.tsx
@@ -1,16 +1,27 @@
+import { Button, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import { PDFViewer } from '@react-pdf/renderer';
+import { PDFDownloadLink } from '@react-pdf/renderer';
 import React from 'react';
 import RecordBook from './RecordBook';
 import { useUserContext } from '../../contexts/UserContext';
 
 const RecordBookShow = (): JSX.Element => {
   const { currentUser } = useUserContext();
+  const today = new Date();
+  const fileName = `${currentUser!.congregation.name} Territory ${
+    today.getMonth() + 1
+  }-${today.getDate()}-${today.getFullYear()}`;
   return (
-    <Box width="100%" height="80vh">
-      <PDFViewer width="100%" height="100%">
-        <RecordBook congregationId={currentUser!.congregation.id} />
-      </PDFViewer>
+    <Box>
+      <Typography variant="h1">Download Records</Typography>
+      <PDFDownloadLink
+        document={<RecordBook congregationId={currentUser!.congregation.id} />}
+        fileName={fileName}
+      >
+        {({ loading }) =>
+          loading ? 'Loading document...' : <Button variant="contained">Download</Button>
+        }
+      </PDFDownloadLink>
     </Box>
   );
 };
